d3f5cbde5324bbafc2965491baf9c8ceef23cc52
Pablo Olmos de Aguilera Corradini <pablo@glatelier.org>
2014-08-29 20:55:40 -0400

    Refactor filter to avoid superclass mismatch

--- lib/nanoc/sprockets/filter.rb
+++ lib/nanoc/sprockets/filter.rb
@@ -1,46 +1,40 @@
-require 'nanoc'
+module Nanoc::Sprockets
+  class Filter < Nanoc::Filter
+    identifier :sprockets
+    type :text
 
-module Nanoc
-  module Filters
-    class Sprockets < Nanoc::Filter
-      VERSION = '0.1.1'
+    def self.environment
+      nanoc_stderr = $stderr
+      $stderr = STDERR
 
-      type :text
+      environment = ::Sprockets::Environment.new(File.expand_path('.')) do |env|
+        assets =  ['javascripts', 'stylesheets', 'images', 'fonts']
+        paths =   ['content/assets/', 'lib/assets/', 'assets/', 'vendor/assets/' ].map{|p| assets.map{|f| "#{p}#{f}" } }.flatten
 
-      def self.environment
-        nanoc_stderr = $stderr
-        $stderr = STDERR
-
-        environment = ::Sprockets::Environment.new(File.expand_path('.')) do |env|
-          assets =  ['javascripts', 'stylesheets', 'images', 'fonts']
-          paths =   ['content/assets/', 'lib/assets/', 'assets/', 'vendor/assets/' ].map{|p| assets.map{|f| "#{p}#{f}" } }.flatten
-
-          paths.each{ |path| env.append_path path }
-        end
-
-        $stderr = nanoc_stderr
-
-        environment
+        paths.each{ |path| env.append_path path }
       end
 
-      def environment
-        @environment ||= self.class.environment
-      end
+      $stderr = nanoc_stderr
 
-      def run(content, params = {})
-        filename = File.basename(@item[:filename])
-
-        environment.css_compressor = params[:css_compressor]
-        environment.js_compressor  = params[:js_compressor]
-
-        if asset = environment[filename]
-          asset.to_s
-        else
-          raise "error locating #{filename} / #{@item[:filename]}"
-        end
-      end
-
+      environment
     end
+
+    def environment
+      @environment ||= self.class.environment
+    end
+
+    def run(content, params = {})
+      filename = File.basename(@item[:filename])
+
+      environment.css_compressor = params[:css_compressor]
+      environment.js_compressor  = params[:js_compressor]
+
+      if asset = environment[filename]
+        asset.to_s
+      else
+        raise "error locating #{filename} / #{@item[:filename]}"
+      end
+    end
+
   end
 end
-
